b04c7d1c0c02c0c72ac96f50f129124f04d389b3
root <root@chlorid.org>
2026-04-19 19:30:22 +0200

    first commit

---
+++ view.blade.php
@@ -0,0 +1,20 @@
+<!--
+  Content on this page will be displayed on your extension's
+  admin page.
+-->
+<DOCTYPE html>
+<html>
+    <body>
+        <hr />
+        <h2>Hello world!</h2>
+        <p>M3Dactyl doesn't need any settings to be changed, you're ready to go!</p>
+        <br />
+        <h1>WARNING</h1>
+        <p>This theme is still in the beta stage, except a lot of bugs and random funny stuff to happen.</p>
+        <br />
+        <h3>Support the developer!</h3>
+        <ul>
+            <li>Ko-fi -> <a href="https://ko-fi.com/optimi">https://ko-fi.com/optimi</a></li>
+        </ul>
+    </body>
+</html>
